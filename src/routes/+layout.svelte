247f1c26761e3d6d5b340d01f7b9b5707c317c8e
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-05-26 16:25:10 +0000

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,93 @@
+<link src="https://assets.nflxext.com/ffe/siteui/fonts/netflix-sans/v3/NetflixSans_W_Rg.woff2" rel="stylesheet">
+<link src="https://assets.nflxext.com/ffe/siteui/fonts/netflix-sans/v3/NetflixSans_W_Md.woff2" rel="stylesheet">
+
+<title>Watch Race to the Stars</title>
+<div class="wrap">
+    <div class="nav">
+            <div class="logoWrap">
+                <a href="/">
+                    <img src="logo.svg" alt="netflix-logo">
+                </a>
+            </div>
+
+            <div class="buttons">
+                <a href="/join" class="joinbutton">JOIN NOW</a>
+                <a href="/login" class="signinbutton">SIGN IN</a>
+            </div>
+    </div>
+    <div>
+        <slot/>
+    </div>
+</div>
+
+
+<style>
+    @font-face {
+        font-family: "Netflix Sans";
+        src: url("https://assets.nflxext.com/ffe/siteui/fonts/netflix-sans/v3/NetflixSans_W_Rg.woff2")
+    }
+    :global(body) {
+        padding: 0px;
+        margin: 0px;
+        font-family: Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif;
+        color: #f1f1f1;
+        -webkit-text-size-adjust: 100%;
+    }
+    .nav {
+        width: 100%;
+        height: 56px;
+        background: black;
+        padding-left: 3%;
+        padding-right: 3%;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        box-sizing: border-box;
+
+    }
+    .logoWrap {
+        fill: #e50914;
+        display: inline-block;
+        text-decoration: none;
+        vertical-align: middle;
+    }
+    .nav img {
+        width: 110px;
+        height: 40px;
+        vertical-align: middle;
+    }
+    .buttons {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: end;
+    }
+    .buttons a {
+        cursor: pointer;
+    }
+    .buttons .signinbutton {
+        font-size: 13px;
+        padding: 9px 12px;
+        border: 1px solid #888;
+        border-radius: 2px;
+        color: #fff;
+        font-weight: 400;
+        line-height: 13px;
+        white-space: nowrap;
+        text-decoration: none;
+        margin: 0 0 0 8px;
+    }
+    .buttons .joinbutton {
+        font-size: 13px;
+        padding: 9px 12px;
+        border: 1px solid #e50914;
+        background: #e50914;
+        border-radius: 2px;
+        color: #fff;
+        font-weight: 400;
+        line-height: 13px;
+        white-space: nowrap;
+        text-decoration: none;
+        margin: 0 0 0 8px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,89 @@
+<div class="head">
+    <div class="content">
+        <div class="title">
+            <h1>Race to the Stars</h1>
+            <div class="quickinfo">
+                <p>2023</p>
+                <span class="bar">|</span>
+                <div class="rating">
+                    TV-14
+                </div>
+                <span class="bar">|</span>
+                <p>2h 21m</p>
+                <span class="bar">|</span>
+                <p>Action & Adventure</p>
+            </div>
+            <div class="desc">
+                <p>
+                    Two ambitious nations find themselves in a military fueled race to the final frontier. This true story follows the United States and Soviet Union through the Space Race. 
+                </p>
+            </div>
+            <p class="staring">
+                <span class="label">Starring:</span> Chris Pratt, Albert, Nancy Pelosi
+            </p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .head {
+        background-image: url("/launch.jpg");
+        width: 100%;
+        height: 100vh;
+        background-size: cover;
+    }
+    .content {
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(90deg, rgb(32, 32, 32), rgb(32,32,32) 30%, transparent);
+        display: flex;
+        align-items: center;
+        justify-content: start;
+    }
+    .title {
+        width: 25%;
+        margin-left: 5%;
+        display: flex;
+        flex-direction: column;
+    }
+    .title h1 {
+        margin: 0px;
+    }
+    .quickinfo {
+        display: flex;
+        flex-direction: row;
+        font-size: 16px;
+        margin-top: 16px;
+        letter-spacing: 0;
+        justify-content: start;
+        align-items: center;
+        color: #a3a3a3
+    }
+    .quickinfo p {
+        margin: 0px;
+    }
+    .quickinfo .bar {
+        margin-top: 16px;
+        all: unset;
+        min-height: 1.5vh;
+        height: 100%;
+        margin-left: 7px;
+        margin-right: 7px;
+        color: #a3a3a3;
+        margin: 0 4px;
+    }
+    .rating {
+        border: 1px solid #a1a1a1;
+        padding: 1px 2px;
+    }
+    .desc p {
+        color: #fff;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 22px;
+        max-width: 100%;
+    }
+    .label {
+        color: #a3a3a3;
+    }
+</style>
